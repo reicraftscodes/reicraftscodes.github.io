a74541fd27cb77cd25efa084bfdf94d44e75f119
May Sanejo <sanejolm@cardiff.ac.uk>
2022-06-24 14:54:12 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b901ced5b0259942e18e.js"
+    "url": "webpack-runtime-83fd2e31528c251a4377.js"
   },
   {
     "url": "framework-a5e9e84b6b86c7f8d29a.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d9f4133aa23a6685ea8620d4e0c2f82b"
+    "revision": "dccf9dc521bf9c4725dbcf3384f1f457"
   },
   {
     "url": "polyfill-8788603fdb224e2314d5.js"
